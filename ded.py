fcbf218b6cdd440e8903a1fd475f364549d39e2b
Aramis <67377571+aramisjose@users.noreply.github.com>
2023-02-01 10:49:59 -0400

    Add files via upload

---
+++ ded.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.title("Análisis de plataformas de aprendizaje") 
+st.markdown("*****")
+
+st.header("Wordcloud Udemy")
+st.image("nubeUdemy.png")
+st.markdown("****")
+st.header("Wordcloud EDX")
+st.image("nubeEdx.png")
+
+
+
